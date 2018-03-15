cac1aa9121e687ad04655bc47b93c271099c9d66
Rubens Carneiro <rubenseuclides@gmail.com>
2018-03-15 00:15:25 -0300

    Histograms and thresholds

--- src/app.py
+++ src/app.py
@@ -1,17 +1,27 @@
-
 import cv2 as cv
 import matplotlib
 matplotlib.use('Agg')
 from matplotlib import pyplot as plt
 
-img = cv.imread('../base/car.jpg')
+img = cv.imread('../base/cars-1.png', 0)
 
-if img is not None:
-    color = ('b', 'g', 'r')
+hist = cv.calcHist([img], [0], None, [256], [0, 256])
+plt.plot(hist)
+plt.xlim([0, 256])
+plt.savefig('../bin/histogram.png')
 
-    for i, col in enumerate(color):
-        histr = cv.calcHist([img], [i], None, [256], [0, 256])
-        plt.plot(histr, color=col)
-        plt.xlim([0, 256])
+equ = cv.equalizeHist(img)
+cv.imwrite('../bin/cars-1-equalized.png', equ)
 
-    plt.savefig('../bin/hist.png')
+hist_eq = cv.calcHist([equ], [0], None, [256], [0, 256])
+plt.plot(hist_eq)
+plt.xlim([0, 256])
+plt.savefig('../bin/equalized-histogram.png')
+
+ret, threshold = cv.threshold(img, 127, 255, cv.THRESH_BINARY)
+mean = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 2)
+gausian = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2)
+
+cv.imwrite('../bin/cars-1-threshold.png', threshold)
+cv.imwrite('../bin/cars-1-mean-threshold.png', mean)
+cv.imwrite('../bin/cars-1-gausian-threshold.png', gausian)
