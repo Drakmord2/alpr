ad8f7d9a48c03ce122c085b044a53ecaf00ad8b5
Rubens Carneiro <rubenseuclides@gmail.com>
2018-03-15 01:05:06 -0300

    Histograms

--- src/app.py
+++ src/app.py
@@ -3,25 +3,36 @@ import matplotlib
 matplotlib.use('Agg')
 from matplotlib import pyplot as plt
 
-img = cv.imread('../base/cars-1.png', 0)
 
-hist = cv.calcHist([img], [0], None, [256], [0, 256])
-plt.plot(hist)
-plt.xlim([0, 256])
-plt.savefig('../bin/histogram.png')
+class App:
+    def __init__(self):
+        img = cv.imread('../base/cars-1.png', 0)
 
-equ = cv.equalizeHist(img)
-cv.imwrite('../bin/cars-1-equalized.png', equ)
+        self.histogram(img, 'Original Image', 121)
 
-hist_eq = cv.calcHist([equ], [0], None, [256], [0, 256])
-plt.plot(hist_eq)
-plt.xlim([0, 256])
-plt.savefig('../bin/equalized-histogram.png')
+        equ = cv.equalizeHist(img)
+        cv.imwrite('../bin/cars-1-equalized.png', equ)
 
-ret, threshold = cv.threshold(img, 127, 255, cv.THRESH_BINARY)
-mean = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 2)
-gausian = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2)
+        self.histogram(equ, 'Equalized Image', 122)
 
-cv.imwrite('../bin/cars-1-threshold.png', threshold)
-cv.imwrite('../bin/cars-1-mean-threshold.png', mean)
-cv.imwrite('../bin/cars-1-gausian-threshold.png', gausian)
+        plt.savefig('../bin/histograms.png')
+
+        ret, threshold = cv.threshold(img, 127, 255, cv.THRESH_BINARY)
+        mean = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 2)
+        gausian = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2)
+
+        cv.imwrite('../bin/cars-1-threshold.png', threshold)
+        cv.imwrite('../bin/cars-1-mean-threshold.png', mean)
+        cv.imwrite('../bin/cars-1-gausian-threshold.png', gausian)
+
+    def histogram(self, img, title, plot_num):
+        hist = cv.calcHist([img], [0], None, [256], [0, 256])
+        plt.subplot(plot_num)
+        plt.plot(hist)
+        plt.xlim([0, 256])
+        plt.title(title)
+        plt.xticks([]), plt.yticks([])
+
+
+if __name__ == '__main__':
+    app = App()
