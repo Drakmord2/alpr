93931a7403b107052db3f3534af261a8aa8a8204
Rubens Carneiro <rubenscarneiro@midiaseducativas.com.br>
2018-05-23 12:02:08 -0300

    Morphology and Representation

--- src/controller/alpr.py
+++ src/controller/alpr.py
@@ -8,6 +8,7 @@ from model.medianf import MedianFilter
 from model.kmeans import Kmeans
 from model.threshold import Threshold
 from model.morphology import Morphology
+from model.representation import Representation
 
 
 class Alpr:
@@ -23,12 +24,10 @@ class Alpr:
 
         filtered = self.noise_filtering(img)
         processed = self.frequency_domain_filtering(filtered)
-        cv.imwrite('../bin/' + self.img_name + '-filtered.png', processed)
-
         threshold = self.segmentation(processed)
-        cv.imwrite('../bin/' + self.img_name + '-threshold.png', threshold)
+        threshold = self.morphology(threshold)
 
-        self.morphology(threshold)
+        self.representation(threshold)
 
     def noise_filtering(self, img):
         print('- Filtering Noise')
@@ -47,6 +46,8 @@ class Alpr:
         th = Threshold(img)
         threshold = th.process_adaptive(window, mean_c)
 
+        cv.imwrite('../bin/' + self.img_name + '-threshold.png', threshold)
+
         return threshold
 
     def frequency_domain_filtering(self, img):
@@ -56,28 +57,109 @@ class Alpr:
         hmf = HomomorphicFilter(img)
         filtered = hmf.filter()
 
-        print('  - Equalizing Histogram')
-        equalized, _, _, _ = Histogram.equalize(filtered)
+        cv.imwrite('../bin/' + self.img_name + '-filtered.png', filtered)
 
-        return equalized
+        return filtered
 
     def morphology(self, img):
         print('- Morphology')
         morph = Morphology()
 
-        kernel = np.ones((3, 3))
+        kernel = np.ones((5, 5))
         kernel = np.array(kernel, np.uint8)
 
         closing = morph.closing(img, kernel)
+
         cv.imwrite('../bin/' + self.img_name + '-closing.png', closing)
+
+        return closing
+
+    def representation(self, img):
+        print('- Representation')
+
+        moments = cv.moments(img, True)
+        hu = cv.HuMoments(moments)
+
+        hustr = ''
+        for i in range(len(hu)):
+            hustr = hustr + '\n\tI' + str(i+1) + ': ' + str(hu[i][0])
+
+        print('   - Hu Moments: ', hustr)
+
+        # Comparacao
+        # hulog = -np.sign(hu) * np.log10(np.abs(hu))
+
+        # print('  - Chain Code')
+        # rep = Representation(img, self.img_name)
+
+        # while True:
+        #     next = rep.chain_code()
+        #
+        #     if not next:
+        #         plt.close()
+        #         break
+
+        return hu
 
     def crop(self, img):
         print('- Cropping')
         img = cv.resize(img, (0, 0), fx=0.8, fy=0.8)
         y, x = img.shape
 
-        img = img[150:y-50, 0:x-100]
+        img = img[150:y-50, 100:x-500]
 
         cv.imwrite('../bin/' + self.img_name + '.png', img)
 
         return img
+
+    def contour(self, img):
+        print('- Contour')
+        img = np.array(img, dtype=np.uint8)
+
+        img_contours, contours, _ = cv.findContours(img, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+        img = cv.cvtColor(img, cv.COLOR_GRAY2BGR)
+
+        for contour in contours:
+            [x, y, w, h] = cv.boundingRect(contour)
+
+            if h > 250 and w > 250:
+                continue
+
+            if h < 40 or w < 40:
+                continue
+
+            colors = self.contour_color()
+            cv.rectangle(img, (x, y), (x + w, y + h), colors, 2)
+
+        cv.imwrite('../bin/' + self.img_name + '-contours.png', img)
+
+        return img_contours
+
+    def save_contours(self, contours):
+        files = []
+        cont = 0
+        for contour in contours:
+            [x, y, w, h] = cv.boundingRect(contour)
+
+            if h > 250 and w > 250:
+                continue
+
+            if h < 40 or w < 40:
+                continue
+
+            box = img[y:y + h, x:x + w]
+
+            file_name = '../bin/' + self.img_name + '-contour-' + str(cont) + '.png'
+            cv.imwrite(file_name, box)
+            files.append(file_name)
+
+            cont += 1
+
+    def contour_color(self):
+        b = np.random.randint(0, 200)
+        g = np.random.randint(0, 200)
+        r = np.random.randint(0, 200)
+
+        colors = (b, g, r)
+
+        return colors
---
+++ src/model/representation.py
@@ -0,0 +1,117 @@
+import numpy as np
+from math import sqrt
+from matplotlib import pyplot as plt
+import cv2 as cv
+
+
+class Representation:
+    def __init__(self, image, image_name):
+        self.image = image
+        self.image_name = image_name
+        self.borders = []
+
+    def chain_code(self):
+        start_point = self.get_start_point()
+
+        if start_point == ():
+            return False
+
+        change_j = [-1, 0, 1,
+                    -1,    1,
+                    -1, 0, 1]
+
+        change_i = [-1, -1, -1,
+                     0,      0,
+                     1,  1,  1]
+
+        directions = [0, 1, 2,
+                      7,    3,
+                      6, 5, 4]
+
+        dir2idx = dict(zip(directions, range(len(directions))))
+
+        # TODO problema
+        border = []
+        chain = []
+        curr_point = start_point
+        for direction in directions:
+            idx = dir2idx[direction]
+            new_point = (start_point[0] + change_i[idx], start_point[1] + change_j[idx])
+
+            if self.image[new_point] != 0:
+                border.append(new_point)
+                chain.append(direction)
+                curr_point = new_point
+                break
+
+        count = 0
+        while curr_point != start_point:
+            b_direction = (direction + 5) % 8
+            dirs_1 = range(b_direction, 8)
+            dirs_2 = range(0, b_direction)
+            dirs = []
+            dirs.extend(dirs_1)
+            dirs.extend(dirs_2)
+
+            for direction in dirs:
+                idx = dir2idx[direction]
+                new_point = (curr_point[0] + change_i[idx], curr_point[1] + change_j[idx])
+
+                if self.image[new_point] != 0:
+                    border.append(new_point)
+                    chain.append(direction)
+                    curr_point = new_point
+                    break
+
+            if count == 1000:
+                break
+
+            count += 1
+
+        self.borders.append(border)
+
+        plt.imshow(self.image, cmap='Greys')
+        plt.plot([i[1] for i in border], [i[0] for i in border])
+        plt.savefig('../bin/' + self.image_name + '-chain.png')
+
+        return True
+
+    def get_start_point(self):
+        xsize, ysize = self.image.shape
+        borders_x = set()
+        borders_y = set()
+
+        for borders in self.borders:
+            borders_x.update({v[0] for v in borders})
+            borders_y.update({v[1] for v in borders})
+
+        start_point = ()
+        for i, row in enumerate(self.image):
+            for j, value in enumerate(row):
+                if i in borders_x and j in borders_y:
+                    continue
+
+                if i+1 in borders_x and j+1 in borders_y:
+                    continue
+
+                if i-1 in borders_x and j-1 in borders_y:
+                    continue
+
+                if i < 1 or i > xsize - 2:
+                    continue
+
+                if j < 1 or i > ysize - 2:
+                    continue
+
+                if value == 255:
+                    start_point = (i, j)
+                    break
+            else:
+                continue
+
+            break
+
+        return start_point
+
+    def skeleton(self):
+        pass
